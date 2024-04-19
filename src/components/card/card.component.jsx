5f509c8c1f3c7dc665da485033c246cc4079a5da
isabella-projects <damyan4oy@gmail.com>
2024-04-19 15:03:55 +0200

    Fetching gender, height and weight as well

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,18 +1,18 @@
-import "./card.styles.css";
+import './card.styles.css';
 
 const Card = ({ monster }) => {
-    const { id, firstName, lastName, email, age } = monster;
+    const { id, firstName, lastName, maidenName, age, gender, height, weight } = monster;
     const fullName = `${firstName} ${lastName}`;
 
     return (
         <div key={id} className="card-container">
-            <img
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                alt={`monster ${fullName}`}
-            />
+            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${fullName}`} />
             <h2>{fullName}</h2>
-            <p>{email}</p>
-            <p>Age: {age * 5}</p>
+            <small>{maidenName}</small>
+            <p>Age: {age}</p>
+            <p>Gender: {gender}</p>
+            <p>Height: {height} cm</p>
+            <p>Weight: {weight} kg</p>
         </div>
     );
 };
